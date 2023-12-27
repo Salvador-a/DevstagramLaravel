40f92d6f949d6dfa35e3f76adeaca5e0eca85296
Salvador-a <salvador.sar614@outlook.com>
2023-12-27 17:03:04 -0600

    Asta buevo aviso

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('styles')
         <title>DevStagram - @yield('titulo')</title>
-        @vite('resources/css/app.css')
-        
+        @livewireStyles
     </head>
     <body class="bg-gray-100">
         
@@ -18,7 +18,7 @@
 
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
                 </nav>
             </div>
         
@@ -32,7 +32,7 @@
 
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos lo derechos reservado
             {{ now()}}
         </footer>
