48600f7bb2cd60e637390fcf062e3b994581826e
Salvador-a <salvador.sar614@outlook.com>
2023-12-26 22:04:16 -0600

    PunTo de Error

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,12 @@
+{{-- Herencia de la plantilla principal --}}
+@extends('layouts.app')
+
+ {{--Continod de h1(Titulo) --}}
+@section('titulo')
+    Nosotros
+@endsection
+
+{{--Contino de br Separacion de secciónes --}}
+@section('contenido')
+    Contenido de esta paguina
+@endsection
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'; // Asegúrate de importar 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/views/**/*.blade.php', // Ruta a todas las vistas Blade
+            ],
             refresh: true,
         }),
     ],
+ 
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    },
 });
