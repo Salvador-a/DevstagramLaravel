c0f18b9038bc7516dabca17ac60107d75e2d3d19
Salvador-a <salvador.sar614@outlook.com>
2023-12-26 23:03:27 -0600

    Se agrego la funcion de yield funciona

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,6 @@
         <title>Laravel</title>
     </head>
     <body >
-        <h1>Hola Mundo en Laravel</h1>
+        <h1>@yield('titulo')</h1>
     </body>
 </html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.app')
+
+@section('titulo')
+Ya estoy hasta la mader    
+@endsection
